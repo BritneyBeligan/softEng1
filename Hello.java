902e9edac7a600c5c2cc01efa7300be2e806c609
Britney Beligan <briyibeligan@gmail.com>
2023-09-03 22:53:35 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is BELIGAN, Britney!");
+System.out.println("That is why developers are highly-paid!");
+
   }
 }
